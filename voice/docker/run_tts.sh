c70ca34a4dac0a671b3e6e2b3ab843c13159950a
Lars Mielke <larsmielke2@gmail.com>
2025-09-04 11:57:28 +0200

    ".gitattributes enable"

--- voice/docker/run_tts.sh
+++ voice/docker/run_tts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Check if the text input is provided as an argument or already in the file
+# Checks if the text input is provided as an argument or already in the file
 if [ "$#" -gt 0 ]; then
   echo "$1" > /app/speak.txt
 fi
